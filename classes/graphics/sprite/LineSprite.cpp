303056c25cb68ef3eab0453d5a2158d1020101a2
Adrian <actesdal@gmail.com>
2017-11-23 16:18:36 +0000

    same
    same

--- classes/graphics/sprite/LineSprite.cpp
+++ classes/graphics/sprite/LineSprite.cpp
@@ -31,10 +31,8 @@ void LineSprite::Render(BYTE* screenPtr, const Rectangle &dest, int posX, int po
 	if (forceNonAlpha) {
 		if (frameRect_.Not_Contained(dest, posX, posY)) {
 			//Do nothing
-			std::cout << "not contained" << std::endl;
 		}
 		else if (frameRect_.Contained_In(dest, posX, posY)) {
-			std::cout << "not contained" << std::endl;
 			//Screen increment and startbyte based on position
 			int endIncrement = (dest.Get_Width() - tRect.Get_Width()) * 4;
 			int startByte = (posX + (posY * dest.Get_Width())) * 4;
@@ -86,71 +84,71 @@ void LineSprite::Render(BYTE* screenPtr, const Rectangle &dest, int posX, int po
 			}
 		}
 	}
-	else {
-		BYTE alpha;
-		if (tRect.Not_Contained(dest, posX, posY)) {
-			//Do nothing
-		}
-		else if (tRect.Contained_In(dest, posX, posY)) {
-			//Screen increment and startbyte based on position
-			int endIncrement = (dest.Get_Width() - tRect.Get_Width()) * 4;
-			int startByte = (posX + (posY * dest.Get_Width())) * 4;
-			scrPtr += startByte;
+	//else {
+	//	BYTE alpha;
+	//	if (tRect.Not_Contained(dest, posX, posY)) {
+	//		//Do nothing
+	//	}
+	//	else if (tRect.Contained_In(dest, posX, posY)) {
+	//		//Screen increment and startbyte based on position
+	//		int endIncrement = (dest.Get_Width() - tRect.Get_Width()) * 4;
+	//		int startByte = (posX + (posY * dest.Get_Width())) * 4;
+	//		scrPtr += startByte;
 
 
-			for (int h{ 0 }; h < tRect.Get_Height(); h++) {
-				for (int w{ 0 }; w < tRect.Get_Width(); w++) {
-					alpha = drawPntr[3];//Getting alpha channel to see if alpha blit (or any blit) is needed
-					if (alpha == 0) {//Invisible
-									 //Do nothing
-					}
-					else if (alpha == 255) {//No transparency
-						memcpy(scrPtr, drawPntr, (sizeof(HAPI_TColour)));
-					}
-					else {//Blended alpha
-						Alpha_Blit(scrPtr, drawPntr);
-					}
-					//Increment pointers by one byte
-					scrPtr += sizeof(HAPI_TColour);
-					drawPntr += sizeof(HAPI_TColour);
-				}
-				//Increment screen pointer to next row
-				scrPtr += endIncrement;
-			}
-		}
-		else {
-			//Screen increment and startbyte
-			int endIncrement = (dest.Get_Width() - tRect.Get_Width()) * 4;
-			int startByte = (std::max(0, posX) + (std::max(0, posY) * dest.Get_Width())) * 4;
+	//		for (int h{ 0 }; h < tRect.Get_Height(); h++) {
+	//			for (int w{ 0 }; w < tRect.Get_Width(); w++) {
+	//				alpha = drawPntr[3];//Getting alpha channel to see if alpha blit (or any blit) is needed
+	//				if (alpha == 0) {//Invisible
+	//								 //Do nothing
+	//				}
+	//				else if (alpha == 255) {//No transparency
+	//					memcpy(scrPtr, drawPntr, (sizeof(HAPI_TColour)));
+	//				}
+	//				else {//Blended alpha
+	//					Alpha_Blit(scrPtr, drawPntr);
+	//				}
+	//				//Increment pointers by one byte
+	//				scrPtr += sizeof(HAPI_TColour);
+	//				drawPntr += sizeof(HAPI_TColour);
+	//			}
+	//			//Increment screen pointer to next row
+	//			scrPtr += endIncrement;
+	//		}
+	//	}
+	//	else {
+	//		//Screen increment and startbyte
+	//		int endIncrement = (dest.Get_Width() - tRect.Get_Width()) * 4;
+	//		int startByte = (std::max(0, posX) + (std::max(0, posY) * dest.Get_Width())) * 4;
 
-			//Texture increment and startbyte
-			int endIncrementT = (tWidth_ - tRect.Get_Width()) * 4;
-			int startByteT = (tRect.Get_Left() + (tRect.Get_Top() * tWidth_)) * 4;
+	//		//Texture increment and startbyte
+	//		int endIncrementT = (tWidth_ - tRect.Get_Width()) * 4;
+	//		int startByteT = (tRect.Get_Left() + (tRect.Get_Top() * tWidth_)) * 4;
 
-			//Setting initial pointer values
-			scrPtr += startByte;
-			drawPntr += startByteT;
+	//		//Setting initial pointer values
+	//		scrPtr += startByte;
+	//		drawPntr += startByteT;
 
-			for (int h{ 0 }; h < tRect.Get_Height(); h++) {
-				for (int w{ 0 }; w < tRect.Get_Width(); w++) {
-					alpha = drawPntr[3];
-					if (alpha == 0) {
-						//Do nothing
-					}
-					else if (alpha == 255) {
-						memcpy(scrPtr, drawPntr, (sizeof(HAPI_TColour)));
-					}
-					else {
-						Alpha_Blit(scrPtr, drawPntr);
-					}
-					scrPtr += sizeof(HAPI_TColour);
-					drawPntr += sizeof(HAPI_TColour);
-				}
-				scrPtr += endIncrement;
-				drawPntr += endIncrementT;
-			}
-		}
-	}
+	//		for (int h{ 0 }; h < tRect.Get_Height(); h++) {
+	//			for (int w{ 0 }; w < tRect.Get_Width(); w++) {
+	//				alpha = drawPntr[3];
+	//				if (alpha == 0) {
+	//					//Do nothing
+	//				}
+	//				else if (alpha == 255) {
+	//					memcpy(scrPtr, drawPntr, (sizeof(HAPI_TColour)));
+	//				}
+	//				else {
+	//					Alpha_Blit(scrPtr, drawPntr);
+	//				}
+	//				scrPtr += sizeof(HAPI_TColour);
+	//				drawPntr += sizeof(HAPI_TColour);
+	//			}
+	//			scrPtr += endIncrement;
+	//			drawPntr += endIncrementT;
+	//		}
+	//	}
+	//}
 }
 
 void LineSprite::Set_Loop(int amount)
